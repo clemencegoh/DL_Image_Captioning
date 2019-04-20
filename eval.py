59bb72c0c9fef0ce49cdd44bca791ba3eebe13a0
Ronald <ronald_heng@mymail.sutd.edu.sg>
2019-04-20 13:13:41 +0800

    Fix bug where full stop was left out.

--- eval.py
+++ eval.py
@@ -49,7 +49,7 @@ def main(args):
             captions = captions.cpu().data.tolist()
             for idx, caption in enumerate(captions):
                 # 1 corresponds to <start>, 2 corresponds to <end>
-                captions[idx] = caption[caption.index(1)+1: caption.index(2)-1]
+                captions[idx] = caption[caption.index(1)+1: caption.index(2)]
                 captions[idx] = [vocab.idx2word[i] for i in captions[idx]]
 
             # Remove start and end from the output
